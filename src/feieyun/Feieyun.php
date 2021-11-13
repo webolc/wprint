becf402cd09522b28e7a954ee860059eb5018598
杨永旭 <516705893@qq.com>
2021-11-13 16:28:28 +0800

    更新打印错误

--- src/feieyun/Feieyun.php
+++ src/feieyun/Feieyun.php
@@ -28,7 +28,7 @@ class Feieyun implements Wprint{
 		$data = [
 			'apiname'=>'Open_printMsg',
 			'sn'=>$number,//打印机编号
-			'content'=>$this->tpl($content),//打印内容
+			'content'=>$content??$this->test_tpl(),//打印内容
 			'times'=>$times//打印次数
 		];
 		return $this->post($data);
@@ -172,179 +172,26 @@ class Feieyun implements Wprint{
 		//"<CB></CB>"为居中放大一倍,"<B></B>"为放大一倍,"<C></C>"为居中,<L></L>字体变高一倍
 		//<W></W>字体变宽一倍,"<QR></QR>"为二维码,"<BOLD></BOLD>"为字体加粗,"<RIGHT></RIGHT>"为右对齐
 	 */
-	protected function tpl($data=[],$type='default'){
-		if ($data){
-			if (isset($data['user_id']) && $data['user_id']){
-				$content = $this->default_tpl($data);
-			}else{
-				$content = $this->index_tpl($data);
-			}
-			
-		}else{
-			$content = $this->test_tpl();
-		}
-		return $content;
-	}
-	/**
-	 * Index模板
-	 */
-	protected function index_tpl($data){
-		$title = isset($data['shop_name'])?$data['shop_name'].'小票':'祥杰收银打印';
-		$content = '<CB>'.$title.'</CB><BR>';
-		if (isset($data['order_id'])){
-			$content .= '单号：'.$data['order_id'].'<BR>';
-		}
-		if (isset($data['mark'])){
-			if ($data['mark']){
-				$content .= '备注：'.$data['mark'].'<BR>';
-			}
-		}
-		$content .= '<BR>';
-		//订单详情
-		if (isset($data['order_detail'])){
-			$content .= '名称　　　　　 单价  数量 金额<BR>';
-			$content .= '--------------------------------<BR>';
-			foreach ($data['order_detail'] as $val){
-				$content .= $val['name'].'　'.$val['price'].'　'.$val['number'].'　'.sprintf("%.2f",$val['price']*$val['number']).'<BR>';
-			}
-			$content .= '--------------------------------<BR>';
-		}
-		if (isset($data['order_money'])){
-			$content .= '合计：'.$data['order_money'].'元<BR><BR>';
-		}
-		//抵扣信息
-		if (isset($data['order_deduct'])){
-			$content .= '抵扣信息　　　　　 　　抵扣金额<BR>';
-			$content .= '--------------------------------<BR>';
-			foreach ($data['order_deduct'] as $val){
-				$content .= $val['deduct_name'].'　　'.$val['money'].'<BR>';
-			}
-			$content .= '--------------------------------<BR>';
-		}
-		//支付方式
-		if (isset($data['order_paytype'])){
-			$content .= '支付方式　　　　　 　　支付金额<BR>';
-			$content .= '--------------------------------<BR>';
-			foreach ($data['order_paytype'] as $val){
-				$content .= $val['show_name'].'　　'.$val['money'].'<BR>';
-			}
-			$content .= '--------------------------------<BR>';
-		}
-		if (isset($data['true_money'])){
-			$content .= '实付：'.$data['true_money'].'元<BR>';
-		}
-		if (isset($data['phone'])){
-			$content .= '商家电话：'.$data['phone'].'<BR>';
-		}
-		if (isset($data['worker_time'])){
-			$content .= '营业时间：'.$data['worker_time'].'<BR>';
-		}
-		$content .= '消费时间：'.date('Y-m-d H:i:s',$data['pay_time']).'<BR>';
-		//把二维码字符串用标签套上即可自动生成二维码
-		if (isset($data['url'])){
-			$content .= '<QR>'.$data['url'].'</QR>';
-		}
-		return $content;
-	}
-	/**
-	 * 默认模板
-	 */
-	protected function default_tpl($data){
-		$title = isset($data['shop_name'])?$data['shop_name'].'小票':'祥杰收银打印';
-		$content = '<CB>'.$title.'</CB><BR>';
-		if (isset($data['order_id'])){
-			$content .= '单号：'.$data['order_id'].'<BR>';
-		}
-		$content .= '时间：'.date('Y-m-d H:i:s',$data['pay_time']).'<BR>';
-		$type = ['快速消费','商品消费','计次充值','计次消费','积分兑换','挂单还款','会员充值','套餐充值','','','人工调整'];
-		$content .= '类型：'.$type[$data['type']].'<BR><BR>';
+	protected function tpl(){
 		
-		//明细
-		if ($data['order_detail']){
-			foreach ($data['order_detail'] as $list){
-				if ($data['type'] == 3){
-					$content .= "{$list['name']}:{$list['price']}*{$list['number']}=".sprintf("%.2f",$list['price']*$list['number'])."元，剩余{$list['surplus_num']}<BR>";
-				}else{
-					$content .= "{$list['name']}:{$list['price']}*{$list['number']}=".sprintf("%.2f",$list['price']*$list['number'])."元<BR>";
-				}
-			}
-		}
-		$content .= '--------------------------------<BR>';
-		$content .= '合计：'.$data['order_money'].'元<BR>';
-		
-		//金额
-		if ($data['true_money']){
-			$content .= '实付：'.$data['true_money'].'元';
-			//抵扣信息：
-			$order_deduct = "";
-			if ($data['order_deduct']){
-				foreach ($data['order_deduct'] as $list){
-					$order_deduct .= "{$list['deduct_name']}{$list['money']}元；";
-				}
-			}
-			//支付方式：
-			$order_paytype = "";
-			if ($data['order_paytype']){
-				foreach ($data['order_paytype'] as $list){
-					$order_paytype .= "{$list['show_name']}{$list['money']}元；";
-				}
-			}
-			if ($order_deduct || $order_paytype){
-				$content .= '（'.$order_deduct.$order_paytype.'）';
-			}
-			$content .= '<BR><BR>';
-		}
-		
-		//余额
-		$content .= '余额积分    　　　　　 　　    <BR>';
-		$content .= '--------------------------------<BR>';
-		if (isset($data['shop_user']['balance']) && $data['shop_user']['balance']){
-			$content .= '本次剩余金额：'.$data['shop_user']['balance'].' 元<BR>';
-		}
-		if (isset($data['score']['last']) && $data['score']['last']){
-			$content .= "本次剩余积分：{$data['score']['last']}<BR>";
-		}
-		if (isset($data['score']['change']) || $data['score']['change']){
-			$content .= "本次消费积分{$data['score']['change']}<BR>";
-		}
-		$content .= '<BR>';
-		
-		if (isset($data['phone'])){
-			$content .= '商家电话：'.$data['phone'].'<BR>';
-		}
-		if (isset($data['worker_time'])){
-			$content .= '营业时间：'.$data['worker_time'].'<BR>';
-		}
-		$content .= '<BR>';
-		$content .= '客户信息    　　　　　 　　    <BR>';
-		$content .= '--------------------------------<BR>';
-		$content .= '称呼：'.$data['shop_user']['true_name'].'<BR>';
-		$content .= '电话：'.$data['shop_user']['phone'].'<BR><BR>';
-		
-		//把二维码字符串用标签套上即可自动生成二维码
-		if (isset($data['url'])){
-			$content .= '<QR>'.$data['url'].'</QR>';
-		}
-		
-		return $content;
 	}
 	/**
 	 * 测试模板
 	 */
 	protected function test_tpl(){
-		$content = '<CB>祥杰收银测试打印</CB><BR>';
+		$content = '<CB>测试打印</CB><BR>';
 		$content .= '名称　　　　　 单价  数量 金额<BR>';
 		$content .= '--------------------------------<BR>';
 		$content .= '测试产品　1.00　1　1.0<BR>';
-		$content .= '老苗医一号产品　10.00　10　100.00<BR>';
-		$content .= '祥杰二号产品　530.00　2　1060.00<BR>';
+		$content .= '一号产品　10.00　10　100.00<BR>';
+		$content .= '二号产品　530.00　2　1060.00<BR>';
 		$content .= '--------------------------------<BR>';
 		$content .= '备注：我没有备注任何信息，请不要留意<BR>';
 		$content .= '合计：1161.00元<BR>';
 		$content .= '商家电话：13888888888888<BR>';
 		$content .= '消费时间：'.date('Y-m-d H:i:s').'<BR>';
 		//把二维码字符串用标签套上即可自动生成二维码
-		$content .= '<QR>http://www.lmyyst.com</QR>';
+		$content .= '<QR>http://www.webolc.com</QR>';
 		return $content;
 	}
 }
